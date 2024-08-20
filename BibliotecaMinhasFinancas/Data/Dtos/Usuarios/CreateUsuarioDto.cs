c6b839650d1686c1ad9d6a2e583b860402752366
Lucas Ferreira <lucas_81303518@live.com>
2024-08-19 21:01:21 -0300

    Endpoint para alterar dados do usuário incluido (/AlterarUsuario)

---
+++ BibliotecaMinhasFinancas/Data/Dtos/Usuarios/CreateUsuarioDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaMinhasFinancas.Dtos.Usuarios
+{
+    public class CreateUsuarioDto
+    {
+        [Required(ErrorMessage = "Campo Nome Completo é obrigatório!")]
+        public string NomeCompleto { get; set; }
+        [Required(ErrorMessage = "Campo Nome é obrigatório!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Campo E-mail é obrigatório!")]
+        public string Email { get; set; }    
+
+        [Required(ErrorMessage = "Campo Senha é obrigatório!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Campo Repetir Senha é obrigatório!")]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+        [Required(ErrorMessage = "Campo Data de Nascimento!")]
+        public DateTime DataNascimento { get; set; }
+        [Required(ErrorMessage = "Campo Celular")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [MaxLength(1048576)]
+        public string FotoBase64 { get; set; } = string.Empty;
+    }
+}
--- BibliotecaMinhasFinancas/Data/Dtos/Usuarios/UpdateUsuarioDto.cs
+++ BibliotecaMinhasFinancas/Data/Dtos/Usuarios/UpdateUsuarioDto.cs
@@ -1,30 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BibliotecaMinhasFinancas.Dtos.Usuarios
+namespace BibliotecaMinhasFinancas.Data.Dtos.Usuarios
 {
     public class UpdateUsuarioDto
     {
         [Required(ErrorMessage = "Campo Nome Completo é obrigatório!")]
-        public string NomeCompleto { get; set; }
-        [Required(ErrorMessage = "Campo Nome é obrigatório!")]
-        public string UserName { get; set; }
+        public string NomeCompleto { get; set; } 
 
         [Required(ErrorMessage = "Campo E-mail é obrigatório!")]
-        public string Email { get; set; }    
-
-        [Required(ErrorMessage = "Campo Senha é obrigatório!")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-        [Required(ErrorMessage = "Campo Repetir Senha é obrigatório!")]
-        [Compare("Password")]
-        public string RePassword { get; set; }
-        [Required(ErrorMessage = "Campo Data de Nascimento!")]
+        public string Email { get; set; }      
         public DateTime DataNascimento { get; set; }
         [Required(ErrorMessage = "Campo Celular")]
         [DataType(DataType.PhoneNumber)]
-        public string PhoneNumber { get; set; }
-        [MaxLength(1048576)]
-        public string FotoBase64 { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; }   
     }
 }
