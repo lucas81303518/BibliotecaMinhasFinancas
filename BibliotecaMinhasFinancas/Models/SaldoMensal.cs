d263af84542c8f92df63f87b2f0e408c53a3a370
Lucas Ferreira <lucas_81303518@live.com>
2024-08-20 23:29:48 -0300

    *Model Saldo Mensal Incluida *SaldoMensagemService incluido *SaldoMensagelController Incluido

---
+++ BibliotecaMinhasFinancas/Models/SaldoMensal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BibliotecaMinhasFinancas.Models
+{
+    public class SaldoMensal
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int Mes { get; set; }
+        [Required]
+        public int Ano { get; set; }
+        [Required]
+        public double ValorTotal { get; set; }  
+        [Required]
+        public int UsuarioId { get; set; }
+        public virtual Usuarios Usuario { get; set; }
+    }
+}
